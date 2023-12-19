ff78ff2b9f16880a20c37372fd7f2ee164422665
srsampson <143277615+srsampson@users.noreply.github.com>
2023-12-19 16:41:38 -0600

    Initial commit

---
+++ src/timing_error_detector.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 Free Software Foundation, Inc.
+ *
+ * Converted to C by Software Toolworks
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <complex.h>
+#include <stdbool.h>
+
+void revert_input_clock(void);
+void sync_reset_input_clock(void);
+void sync_reset(void);
+void create_timing_error_detector(void);
+void destroy_timing_error_detector(void);
+void ted_input(complex float *);
+void revert(bool);
+complex float getMiddleSample(void);
+float get_error(void);
+int get_inputs_per_symbol(void);
+
+#ifdef __cplusplus
+}
+#endif
+
